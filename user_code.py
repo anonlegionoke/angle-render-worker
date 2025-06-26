78b68f813301e83401ce99cb897fceec28a19610
anonlegionoke <anonlegionoke@protonmail.com>
2025-06-26 12:16:59 +0530

    Remove CircleScene implementation from user_code.py

--- user_code.py
+++
@@ -1,8 +0,0 @@
-from manim import *
-
-class CircleScene(Scene):
-    def construct(self):
-        circle = Circle()           # Create a circle
-        circle.set_fill(BLUE, opacity=0.5)  # Set the color and transparency
-        self.play(Create(circle))   # Show the circle on screen
-        self.wait(1)                # Pause for a second
